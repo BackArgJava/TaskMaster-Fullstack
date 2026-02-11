1c68de47e7b943ad5f46620eb1f252d25ad9c3fc
Santi <santi.bedran@gmail.com>
2026-02-11 02:22:53 -0300

    Allow Vercel domain

--- backend/src/main/java/com/taskmaster/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/taskmaster/backend/config/SecurityConfig.java
@@ -42,8 +42,11 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        // ALLOW REACT (Make sure this matches your Vite port)
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://task-master-fullstack.vercel.app"
+        ));
 
         // ALLOW COMMANDS
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
